39b4f95317bceaadce95ba572d96efbdd5c5a611
tangtao <tangtao@gmail.com>
2010-12-20 11:07:25 +0800

    refactoring parser base class

--- lib/sinatra-sdb/parser/query_expression.rb
+++ lib/sinatra-sdb/parser/query_expression.rb
@@ -2,14 +2,13 @@ require 'set'
 
 module SDB
   # Uses the lexer/parser/evaluator to perform the query and do simple paging
-  class QueryExecutor
-    ERROR_MARKER = ">>>"
-  
+  class QueryExecutor < BaseExecutor
+
     def initialize
       @lexer = Dhaka::Lexer.new(QueryLexerSpec)
       @parser = Dhaka::Parser.new(QueryGrammar)
     end
-    
+  
     # Execute the query
     def do_query(query, domain, max = 100, token = 0)
       parse_result = @parser.parse(@lexer.lex(query))
@@ -38,22 +37,6 @@ module SDB
       end
     end
     
-    # From dhaka examples
-    def parse_error_message(unexpected_token, program)
-      if unexpected_token.symbol_name == Dhaka::END_SYMBOL_NAME
-        "Unexpected end of file."
-      else
-        "Unexpected token #{unexpected_token.symbol_name}:\n#{program.dup.insert(unexpected_token.input_position - 1, ERROR_MARKER)}"
-      end
-    end
-    
-    def tokenize_error_message(unexpected_char_index, program)
-      "Unexpected character #{program[unexpected_char_index - 1].chr}:\n#{program.dup.insert(unexpected_char_index - 1, ERROR_MARKER)}"
-    end
-    
-    def evaluation_error_message(evaluation_result, program)
-      "#{evaluation_result.exception}:\n#{program.dup.insert(evaluation_result.node.tokens[0].input_position, ERROR_MARKER)}"
-    end
   
   end
   
--- lib/sinatra-sdb/parser/select_executor.rb
+++ lib/sinatra-sdb/parser/select_executor.rb
@@ -2,8 +2,7 @@ require 'set'
 
 module SDB
   # Uses the lexer/parser/evaluator to perform the query and do simple paging
-  class SelectExecutor
-    ERROR_MARKER = ">>>"
+  class SelectExecutor < BaseExecutor
   
     def initialize
       logger           = Logger.new(STDOUT)
@@ -41,23 +40,6 @@ module SDB
       else
         return results,count
       end
-    end
-    
-    # From dhaka examples
-    def parse_error_message(unexpected_token, program)
-      if unexpected_token.symbol_name == Dhaka::END_SYMBOL_NAME
-        "Unexpected end of file."
-      else
-        "Unexpected token #{unexpected_token.symbol_name}:\n#{program.dup.insert(unexpected_token.input_position - 1, ERROR_MARKER)}"
-      end
-    end
-    
-    def tokenize_error_message(unexpected_char_index, program)
-      "Unexpected character #{program[unexpected_char_index - 1].chr}:\n#{program.dup.insert(unexpected_char_index - 1, ERROR_MARKER)}"
-    end
-    
-    def evaluation_error_message(evaluation_result, program)
-      "#{evaluation_result.exception}:\n#{program.dup.insert(evaluation_result.node.tokens[0].input_position, ERROR_MARKER)}"
     end
   
   end
